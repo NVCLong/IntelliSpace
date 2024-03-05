cc2bfd6bcd0a59cc22ef09888661272fc278f47e
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2024-03-05 19:16:36 +0700

    Initial commit

---
+++ src/main/java/com/webapp/intelligentworkspace/IntelligentWorkSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.intelligentworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentWorkSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentWorkSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/intelligentworkspace/IntelligentWorkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.intelligentworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentWorkSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
