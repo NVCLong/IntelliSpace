0954f168fe8b01ed13ea779ead3e2cf52c0cbc9e
Nguyen Duy <thanhduy1706@gmail.com>
2024-03-11 22:22:28 +0700

    Update sign in and sign up without TailwindCSS

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>IntelliSpace</h1>
-    </div>
-  );
+  return <div></div>;
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -47,7 +47,26 @@ module.exports = {
       borderRadius: {
         "5xl": "40px",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
