eb8bf7bb67f287306faeb4d167412c24e5402183
Nguyen Duy <thanhduy1706@gmail.com>
2024-03-19 00:06:00 +0700

    Refactor Sidebar component styles

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -19,14 +19,14 @@ const Sidebar: React.FC = () => {
   return (
     <div
 
-      className={`${open ? "w-60" : "w-20"
-        }  z-10 flex-col h-screen text-gray-700 shadow-xl backdrop-blur-2xl transition-all duration-500 p-5 pt-8`}
+      className={`${open ? "w-60" : "w-24 p-2"
+        } z-10 flex-col h-screen  shadow-xl backdrop-blur-2xl transition-all duration-300 p-5 pt-8`}
     >
       <div className="h-full overflow-y-auto lg:block">
 
         <img
           src="/control.png"
-          className={`absolute cursor-pointer rounded-full -right-3 top-7 w-7 border-2 border-white transition duration-200 transform hover:scale-110
+          className={`absolute cursor-pointer rounded-full -right-3 top-8 w-6 transition duration-200 transform hover:scale-110
           ${!open && "rotate-180"}`}
           alt="control_icon"
           onClick={() => setOpen(!open)}
@@ -34,29 +34,30 @@ const Sidebar: React.FC = () => {
 
         <div className="flex space-x-2 transition">
           <Link href="/" >
-            <Image src="/IntelliSpace.png" alt="logo" width={30} height={30} className={`cursor-pointer duration-200 hoverScale
-            ${open && "rotate-[360deg]"}`} />
+            <Image src="/IntelliSpace.png" alt="logo" width={30} height={30} className={`cursor-pointer duration-300 ml-7 w-8 h-8
+            ${!open && ""}`} />
           </Link>
-          <span className={`text-2xl font-bold transition duration-200 ${!open && "hidden transition"
+          <span className={`text-2xl font-bold transition duration-300 ${!open && "hidden transition"
             }`}>IntelliDrive</span>
         </div>
 
-        <div className="p-4 space-y-2">
+        <div className="p-4 space-y-2 overflow-hidden">
           {navItems.map((item, index) => (
             <div
               key={index}
-              className="flex items-center p-4 mt-8 transition duration-200 ease-linear transform rounded-lg cursor-pointer hover:bg-white-100 focus:bg-slate-50 focus:outline-none active:bg-slate-50 hover:text-white hover:bg-white/10 focus:bg-white/10 active:bg-white/10 w-fit"
+              className="flex items-center p-4 mt-8 transition duration-100 ease-linear transform rounded-lg cursor-pointer focus:bg-slate-50 focus:outline-none active:bg-slate-50 hover:text-white hover:bg-white/10 focus:bg-white/10 active:bg-white/10 w-fit"
             >
               <div
                 className={`${!open ? "w-full flex justify-center" : ""
-                  } transition-all duration-200`}
+                  } transition-all duration-100`}
               >
-                <item.icon className="w-6 h-6 text-gray-500 cursor-pointer" />
+                <item.icon className={`${!open ? "w-6 h-6 cursor-pointer flex justify-center " : ""
+                  } transition-all duration-200 w-6 h-6`} />
               </div>
               <Link href={item.path}>
                 <span
                   className={`${!open && "hidden"
-                    } relative pl-3 duration-200 after:block after:content-[''] after:absolute after:h-[3px] after:bg-purple-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center`}
+                    } relative pl-3 duration-200 after:block after:content-[''] after:absolute after:h-[3px] after:bg-purple-400 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-200 after:origin-center`}
                 >
                   {item.name}
                 </span>
