2c1c336358d642ea051a841bcde2a3550f66428a
Nguyen Duy <thanhduy1706@gmail.com>
2024-04-03 13:40:57 +0700

    Update FolderList component styles

--- frontend/components/FolderList.tsx
+++ frontend/components/FolderList.tsx
@@ -19,8 +19,8 @@ const FolderList: React.FC<FolderListProps> = ({ folders }) => {
         }
     }
     return (
-        <div>
-            <div className="mt-10 ml-20 ">
+        <div className="container px-4 mx-auto ml-12">
+            <div className="mt-20 mb-0">
                 <button
                     className="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-full shadow-md cursor-pointer hoverScale"
                     onClick={handleNewFolder()}
@@ -29,7 +29,7 @@ const FolderList: React.FC<FolderListProps> = ({ folders }) => {
                     <span className="ml-4 font-semibold">Create folder</span>
                 </button>
             </div>
-            <div className="grid h-12 grid-cols-8 pt-20 pl-20 gap-7 w-15">
+            <div className="grid grid-cols-1 gap-4 pt-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {folders.map((folder) => (
                     <div key={folder.id} className="flex items-center p-4 bg-white border rounded-md shadow-md cursor-pointer hoverScale">
                         <MdFolder className="text-blue-400" size={24} />
