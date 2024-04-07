890a5f9243669651377d69f04a4f6677c7c8c525
Nguyen Duy <thanhduy1706@gmail.com>
2024-04-07 19:48:52 +0700

    Refactor FolderList component and add create folder functionality

--- frontend/components/FolderList.tsx
+++ frontend/components/FolderList.tsx
@@ -19,13 +19,24 @@ const FolderList: React.FC<FolderListProps> = ({ folders }) => {
         }
     }
     return (
-        <div className="pl-16 space-y-4 pt-28">
-            {folders.map((folder) => (
-                <div key={folder.id} className="flex items-center p-4 bg-white border rounded-md shadow-md cursor-pointer hoverScale">
-                    <MdFolder className="text-blue-400" size={24} />
-                    <span className="ml-4">{folder.name}</span>
-                </div>
-            ))}
+        <div>
+            <div className="mt-10 ml-20 ">
+                <button
+                    className="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-full shadow-md cursor-pointer hoverScale"
+                    onClick={handleNewFolder()}
+                >
+                    <FiFolderPlus size={24} />
+                    <span className="ml-4 font-semibold">Create folder</span>
+                </button>
+            </div>
+            <div className="grid grid-cols-1 pt-20 pl-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-8 gap-7">
+                {folders.map((folder) => (
+                    <div key={folder.id} className="flex items-center p-4 bg-white border rounded-md shadow-md cursor-pointer hoverScale">
+                        <MdFolder className="text-blue-400" size={24} />
+                        <span className="ml-4">{folder.name}</span>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
