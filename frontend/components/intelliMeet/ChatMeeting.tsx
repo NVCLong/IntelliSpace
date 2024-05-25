757122e617ec67c26c0c8812bc2fb40eef1e1790
Nguyen Duy <thanhduy1706@gmail.com>
2024-05-25 14:35:28 +0700

    chore: Add SockJS, sockjs-client, and stompjs dependencies

--- frontend/components/intelliMeet/ChatMeeting.tsx
+++ frontend/components/intelliMeet/ChatMeeting.tsx
@@ -1,11 +1,33 @@
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import SockJS from 'sockjs-client'
+import Stomp from 'stompjs'
+import { useState } from "react";
 
-const handleSendMessage = () => {
-    console.log('Message sent')
-}
+
 
 const ChatMeeting = () => {
+
+    const [message, setMessage] = useState("")
+    const [nickName, setNickName]= useState("")
+    const [stompClient, setStompClient]= useState("")
+    let sock = new SockJS("http://localhost:8888/api/ws-message");
+    let stomp= Stomp.over(sock);
+
+
+    sock.onopen = function() {
+        console.log('open');
+        sock.send('test');
+    };
+
+    sock.onmessage = function(e) {
+        console.log('message', e.data);
+        sock.close();
+    };
+
+    sock.onclose = function() {
+        console.log('close');
+    };
     return (
         <div className="flexCenter">
             <Input type="text" placeholder="Type a message" />
